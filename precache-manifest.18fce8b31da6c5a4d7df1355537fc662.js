84a543430b45797d070818892b0ef374525583e0
Angad Singh <angadsingh567890@gmail.com>
2019-01-11 04:04:18 +0530

    Updates

---
+++ precache-manifest.18fce8b31da6c5a4d7df1355537fc662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5e3f2904989951119a4332cf9acd69",
+    "url": "/static/media/angad.fa5e3f29.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c8d4652206a41d2c07b3",
+    "url": "/static/js/main.c8d46522.chunk.js"
+  },
+  {
+    "revision": "9760d9ab5b9ff4022f18",
+    "url": "/static/js/1.9760d9ab.chunk.js"
+  },
+  {
+    "revision": "c8d4652206a41d2c07b3",
+    "url": "/static/css/main.ab0a2d3f.chunk.css"
+  },
+  {
+    "revision": "6f0350bb07981ca657af29844ca7d1e0",
+    "url": "/index.html"
+  }
+];
