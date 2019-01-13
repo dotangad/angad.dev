aca02783577a773b971326b58e2af010189691fa
Angad Singh <angadsingh567890@gmail.com>
2019-01-13 06:12:33 +0530

    Move onThemeToggle to a method on the class ThemeToggle

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -39,82 +39,112 @@ const Switch = styled.div`
   z-index: 100001;
 `
 
-const onThemeToggle = e => {
-  window.darkTheme = !window.darkTheme
-  const switchDiv = e.target.classList.contains('toggle')
-    ? e.target.childNodes[0]
-    : e.target
-  switchDiv.style.transform === ''
-    ? (switchDiv.style.transform = 'translateX(12px)')
-    : (switchDiv.style.transform = '')
+export default class ThemeToggle extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      darkTheme: false
+    }
+  }
 
-  if (window.darkTheme) {
-    document.querySelectorAll('*').forEach((e, i, all) => {
-      e.style.color = '#eee'
-    })
+  onThemeToggle = e => {
+    window.darkTheme = !window.darkTheme
+    const switchDiv = e.target.classList.contains('toggle')
+      ? e.target.childNodes[0]
+      : e.target
+    window.darkTheme
+      ? (switchDiv.style.transform = 'translateX(12px)')
+      : (switchDiv.style.transform = '')
 
-    const body = document.body
-    body.style.background = '#000000'
-
-    document.querySelector('img').style.boxShadowColor =
-      'box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1), 0 5px 15px rgba(256, 256, 256, 0.1);'
-
-    document
-      .querySelectorAll('.toggle')
-      .forEach((e, i, all) => (e.style.background = '#ffffff80'))
-    document
-      .querySelectorAll('.switch')
-      .forEach((e, i, all) => (e.style.background = '#000'))
-
-    document.querySelectorAll('a').forEach((e, i, all) => {
-      e.style.borderColor = '#ddd'
-      e.addEventListener('mouseenter', () => {
-        e.style.borderColor = config.baseColor
-        e.style.color = config.baseColor
-      })
-      e.addEventListener('mouseleave', () => {
-        e.style.borderColor = '#ddd'
+    if (window.darkTheme) {
+      document.querySelectorAll('*').forEach((e, i, all) => {
         e.style.color = '#eee'
       })
-    })
-  } else {
-    document.querySelectorAll('*').forEach((e, i, all) => {
-      e.style.color = '#333'
-    })
 
-    const body = document.body
-    body.style.background = '#00000010'
+      const body = document.body
+      body.style.background = '#000000'
 
-    document.querySelector('img').style.boxShadowColor =
-      'box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1), 0 5px 15px rgba(0, 0, 0, 0.1);'
+      document.querySelector('img').style.boxShadow =
+        'box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1), 0 5px 15px rgba(256, 256, 256, 0.1);'
 
-    document
-      .querySelectorAll('.toggle')
-      .forEach((e, i, all) => (e.style.background = '#00000080'))
-    document
-      .querySelectorAll('.switch')
-      .forEach((e, i, all) => (e.style.background = '#fff'))
+      document
+        .querySelectorAll('.toggle')
+        .forEach((e, i, all) => (e.style.background = '#ffffff'))
+      document
+        .querySelectorAll('.switch')
+        .forEach((e, i, all) => (e.style.background = '#000'))
 
-    document.querySelectorAll('a').forEach((e, i, all) => {
-      e.style.borderColor = '#333'
-      e.addEventListener('mouseenter', () => {
-        e.style.borderColor = config.baseColor
-        e.style.color = config.baseColor
+      document.querySelectorAll('a').forEach((e, i, all) => {
+        e.addEventListener('mouseenter', () => {
+          e.style.color = config.baseColor
+        })
+        e.addEventListener('mouseleave', () => {
+          e.style.color = '#eee'
+        })
       })
-      e.addEventListener('mouseleave', () => {
-        e.style.borderColor = '#333'
+
+      document.querySelectorAll('a').forEach((e, i, all) => {
+        e.style.borderColor = '#ddd'
+        e.addEventListener('mouseenter', () => {
+          e.style.borderColor = config.baseColor
+          e.style.color = config.baseColor
+        })
+        e.addEventListener('mouseleave', () => {
+          e.style.borderColor = '#ddd'
+          e.style.color = '#eee'
+        })
+      })
+    } else {
+      document.querySelectorAll('*').forEach((e, i, all) => {
         e.style.color = '#333'
       })
-    })
-  }
-}
 
-export default () => (
-  <ToggleContainer className="toggle-container">
-    <Icon className="fas fa-sun" />
-    <Toggle className="toggle" onClick={onThemeToggle}>
-      <Switch className="switch" />
-    </Toggle>
-    <Icon className="fas fa-moon" />
-  </ToggleContainer>
-)
+      const body = document.body
+      body.style.background = 'white'
+
+      document.querySelector('img').style.boxShadow =
+        'box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1), 0 5px 15px rgba(0, 0, 0, 0.1);'
+
+      document
+        .querySelectorAll('.toggle')
+        .forEach((e, i, all) => (e.style.background = '#00000080'))
+      document
+        .querySelectorAll('.switch')
+        .forEach((e, i, all) => (e.style.background = '#fff'))
+
+      document.querySelectorAll('a').forEach((e, i, all) => {
+        e.addEventListener('mouseenter', () => {
+          e.style.color = config.baseColor
+        })
+        e.addEventListener('mouseleave', () => {
+          e.style.color = '#333'
+        })
+      })
+
+      document.querySelectorAll('a').forEach((e, i, all) => {
+        e.style.borderColor = '#333'
+        e.addEventListener('mouseenter', () => {
+          e.style.borderColor = config.baseColor
+          e.style.color = config.baseColor
+        })
+        e.addEventListener('mouseleave', () => {
+          e.style.borderColor = '#333'
+          e.style.color = '#333'
+        })
+      })
+    }
+  }
+
+  render = () => (
+    <ToggleContainer className="toggle-container">
+      <Icon className="fas fa-sun" />
+      <Toggle className="toggle" onClick={this.onThemeToggle}>
+        <Switch
+          className="switch"
+          style={{ transform: window.darkTheme ? 'transformX(12px)' : '' }}
+        />
+      </Toggle>
+      <Icon className="fas fa-moon" />
+    </ToggleContainer>
+  )
+}
